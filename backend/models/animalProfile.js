474c7f0e8b6c90aac92c9d564554ce481a0a2d2e
BHutch33 <120543890+BHutch33@users.noreply.github.com>
2025-03-31 10:44:00 -0400

    Update animalProfile.js
    Updated animalProfile schema to match mock animals.

--- backend/models/animalProfile.js
+++ backend/models/animalProfile.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const animalSchema = new Schema({
+  animalID: {type: Integer },
   name: { type: String, required: true },
+  species: {type: String },
   breed: { type: String, required: true },
   age: { type: Number, required: true },
-  image: { type: String },
-  dateAdded: { type: Date, default: Date.now },
-  medicalHistory: { type: String },
+  sex: {type: String },
+  intakeDate: { type: Date, default: Date.now },
+  medicalRecords: { type: String },
+  photo: { type: String },
 });
 
 module.exports = mongoose.model('Animal', animalSchema);
